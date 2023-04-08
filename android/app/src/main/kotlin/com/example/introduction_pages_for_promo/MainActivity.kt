75759fe121e868c399380270031bf59090f3459b
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-04-08 17:29:42 +0300

    Workspace Created

---
+++ android/app/src/main/kotlin/com/example/introduction_pages_for_promo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.introduction_pages_for_promo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_pages_for_promo/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
